8b87476fcf9c4505f26127e595346c7aa6b1a39a
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-08-06 17:30:09 +0530

    Made the UI of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'service.dart' show response;
+import 'dart:convert';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:ui';
 
 void main() => runApp(MyApp());
 
@@ -17,26 +21,121 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  ByteData imgBytes;
+  int res = 0;
+  List<Offset> _points = <Offset>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.yellowAccent,
-        child: RaisedButton(
-          onPressed: () {
-            response('imgData');
-          },
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$res',
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.black,
+              child: GestureDetector(
+                onPanUpdate: (DragUpdateDetails details) {
+                  setState(() {
+                    RenderBox object = context.findRenderObject();
+                    Offset _localPosition =
+                        object.globalToLocal(details.localPosition);
+                    _points = new List.from(_points)..add(_localPosition);
+                  });
+                },
+                onPanEnd: (DragEndDetails details) => _points.add(null),
+                child: CustomPaint(
+                  painter: Signature(points: _points),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: RaisedButton(
+                    child: Text('Predict'),
+                    onPressed: generateImage,
+                    color: Colors.green,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: RaisedButton(
+                    child: Text('Clear'),
+                    onPressed: () => _points.clear(),
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+
+  void generateImage() async {
+    final recorder = ui.PictureRecorder();
+    final canvas =
+        Canvas(recorder, Rect.fromPoints(Offset(0.0, 0.0), Offset(200, 200)));
+
+    final stroke = Paint()
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawRect(Rect.fromLTWH(0.0, 0.0, 200, 200), stroke);
+
+    final picture = recorder.endRecording();
+    final img = await picture.toImage(96, 96);
+    final pngBytes = await img.toByteData(format: ImageByteFormat.png);
+
+    Uint8List audioUint8List = pngBytes.buffer
+        .asUint8List(pngBytes.offsetInBytes, pngBytes.lengthInBytes);
+    List<int> audioListInt = audioUint8List.cast<int>();
+    String base64Image = base64Encode(audioListInt);
+    print(base64Image);
+    response(base64Image).then((vars) {
+      setState(() {
+        res = json.decode(vars)['prediction'];
+      });
+    }).catchError((err) {
+      print(err);
+    });
+  }
+}
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.white
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 15.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
 }
--- lib/service.dart
+++ lib/service.dart
@@ -1,7 +1,10 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<String> response(String imgData) async {
   var url = 'https://pure-stream-49009.herokuapp.com';
-  var prediction = await http.post(url, body: {'image': '$imgData'});
+  var prediction = await http.post(url,
+      headers: {"Content-Type": "application/json"},
+      body: json.encode({"image": "$imgData"}));
   return prediction.body;
 }
